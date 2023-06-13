9da0ba3343041a852cecc63ce601afed402daa5b
monique <98707165+nickellen@users.noreply.github.com>
2023-06-13 16:16:18 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from classes.simulador import *
+
+Simulador().testarMetodos(10000, 72, 6)
+
+#Simulador().rodarSimulacoes(
+#    quantidadeSimulacoes = 10000,
+#    qtdAssento = 72,
+#    qtdColuna = 6,
+#    metodo = 1,
+#    useDB = True,
+#    visual = False
+#)

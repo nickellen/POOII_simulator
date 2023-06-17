001c8d94a09f1eb4ac6efb30976cf140f7c03912
monique <98707165+nickellen@users.noreply.github.com>
2023-06-17 18:26:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
 from classes.simulador import *
 
-Simulador().testarMetodos(10000, 72, 6)
+def setBool(variavel):
+    if variavel =="True":
+        variavel = True
+    else:
+        variavel = False
+    return variavel
 
-#Simulador().rodarSimulacoes(
-#    quantidadeSimulacoes = 10000,
-#    qtdAssento = 72,
-#    qtdColuna = 6,
-#    metodo = 1,
-#    useDB = True,
-#    visual = False
-#)
+def main():
+
+    while True:
+
+        op = int(input("\n1 - Testar métodos de embarque\n2 - Encontrar melhor sequencia de embarque\n"))
+        os.system('cls')
+
+        if (op==1):
+            quantidadeSim = int(input("Quantidade de simulações: "))
+            Simulador().testarMetodos(quantidadeSim, 72, 6)
+
+        elif (op==2):
+
+            quantidadeSim = int(input("Quantidade de simulações: "))
+            metodo = int(input("Informe o método:\n1 - Random\n2 - Steffen\n3 - Wilma\n4 - Blocos\n5 - De trás para frente\n"))
+            bd = input("Deseja salvar as simulações (True ou False): ")
+            visual = input("Deseja ver a representação visual (True ou False): ")
+    
+            bd = setBool(bd)
+            visual = setBool(visual)
+        
+            Simulador().rodarSimulacoes(
+                quantidadeSimulacoes = quantidadeSim,
+                qtdAssento = 72,
+                qtdColuna = 6,
+                metodo = metodo,
+                useDB = bd,
+                visual = visual
+            )
+
+main()
